74a25c6f7131e67783b4c7044a3f6d4ce14f75ba
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-26 19:21:41 +0300

    commit tree b316d38296f027b2073b083d91637ca6d68349cf

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 // write your code here
+document.addEventListener("DOMContentLoaded", init);
+function init(){
+
+function getAllMenu() {
+fetch("http:localhost:3000/ramens")  
+.then((res)=>res.json())
+.then((ramens)=>{
+    ;
+        
+    });
+   
+}
+
+function renderMenu(menu){
+let img = document.createElement("img")
+img.innerHTML = `
+<img src = "${menu.image}" alt = "Images">`
+document.getElementById("ramen-menu").appendChild(img);
+console.log(img)
+}
+function initialize(){
+    ramen.forEach(menu => renderMenu(menu))
+}
+getAllMenu();
+initialize()
+}
